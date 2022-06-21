a3888333b71b0839b65b4a57e7a63711f28fd398
shubhammane8668 <shubhsmane@gmail.com>
2022-06-21 18:40:49 +0530

    Added: checking employee present or absent

---
+++ src/employeepresentabsent1.java
@@ -0,0 +1,20 @@
+
+public class employeepresentabsent1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		int isPresent =(int) (Math.random()*100)%3;
+		System.out.println(isPresent);
+		if(isPresent==0) {
+			System.out.println("employee is absent");
+		}
+		else if(isPresent==1){
+			System.out.println("employee is present");
+		}
+		else {
+			System.out.println("employee is present for half day");	
+		}
+	}
+
+}
